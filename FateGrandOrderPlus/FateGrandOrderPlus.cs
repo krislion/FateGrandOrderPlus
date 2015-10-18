86688d38371efd6d9b07151c60fc4bc671183728
Kris Lion <kris.lion@gmail.com>
2015-10-17 18:49:27 -0700

    initial commit

---
+++ FateGrandOrderPlus/FateGrandOrderPlus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FateGrandOrderPlus
+{
+    public partial class FateGrandOrderPlus : Form
+    {
+        public FateGrandOrderPlus()
+        {
+            InitializeComponent();
+        }
+    }
+}
